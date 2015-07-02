01ca63cc70f0c57a9058fd449db80f38f2ff5a57
Masaaki Morishita <ikasama3.0@gmail.com>
2015-07-02 22:49:53 +0900

    first implementation

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+var postcss = require('postcss')
+
 module.exports = function () {
+    this.postcss = function (processors, opts) {
+        return this.filter((data) => {
+            try {
+                return postcss(processors).process(data, opts).css
+            } catch (e) {
+                plugin: 'postcss',
+                error: e
+            }
+        }).notify('plugin_run', {plugin: 'postcss'})
+    }
 }
