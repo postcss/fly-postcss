ea464161faf88fecd005a8313c6a9e0e7dfade0a
Masaaki Morishita <ikasama3.0@gmail.com>
2015-08-22 20:52:55 +0900

    update to fly v0.7 new API

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 module.exports = function () {
-  this.filter("postcss", (data, processors, options) => {
-    try {
-      return require("postcss")(processors).process(data.toString(), options).css
-    } catch (e) { throw e }
-  })
+  this.filter("postcss", (data, processors, options) => ({
+    css: require("postcss")(processors).process(data.toString(), options).css
+  }))
 }
