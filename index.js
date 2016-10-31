74f9c42ef959210c5bcd8f8271fde53c945f2057
Oleh Kuchuk <oleh.kuchuk@globallogic.com>
2016-10-31 18:30:50 +0200

    rewritten with support of fly v2

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
-var postcss = require("postcss")
-var assign = require("object-assign")
+'use strict'
 
-module.exports = function() {
+var postcss = require('postcss')
+var assign = require('object-assign')
+
+module.exports = function () {
     var self = this
-    
-    self.filter("postcss", function (data, config) {
-        // Combine our config with defaults using Object.assign
-        config = assign({}, {plugins: [], options: {}}, config)
 
-        // Return our promise out so we can wrap this async promise-based API
-        return new Promise(function (resolve, reject) {
-            // Pull in the plugins list
-            postcss(config.plugins)
-                // Process the CSS with PostCSS and our options object passed to PostCSS
-                .process(data.toString(), config.options)
-                .then(function (result) {
-                    // Resolve our CSS the way Fly expects it
-                    return resolve({css: result.css})
-                })
-                .catch(function (err) {
-                    self.emit("plugin_error", {
-                      plugin: "fly-postcss",
-                      error: err
-                    })
-                })
+    self.plugin('postcss', {}, function * (file, opts) {
+      opts = assign({}, {plugins: [], options: {}}, opts)
+
+      try {
+        var processor = postcss(opts.plugins)
+        var result = yield processor.process(file.data.toString(), opts)
+
+        file.data = new Buffer(result.css)
+      } catch (error) {
+        self.emit('plugin_error', {
+          plugin: 'fly-postcss',
+          error: error
         })
+      }
     })
 }
